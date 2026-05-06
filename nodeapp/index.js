17cea1432da1ba4177b4b7cadec73e12ab03c87c
dinelkagui <dinelka@guisrilanka.com>
2026-05-06 13:40:56 +0530

    changed index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": CI/CD pipeline docker automation"}');
+  res.send('{CI/CD pipeline docker automation"}');
 });
 
 app.listen(process.env.PORT || 3000, function () {
